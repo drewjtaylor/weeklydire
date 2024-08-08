a83d4f0a85a85056b766a18bff4b62298822f75d
Andrew Taylor <drew.j.taylor1@gmail.com>
2024-08-08 16:54:36 -0400

    Users who are premium cannot reach joinup page
    The JoinUp page checks the current user.
    
    If the current user is undefined, it means they
    are not signed in, and it will request they do so.
    
    If the current user is not a premium user, it will
    display a message and paypal/venmo links to
    complete the transaction.
    
    If the user is already a premium user, it will
    display a message informing them of that fact.
    
    The rest of the application should not allow a
    premium user to find any links to the /joinup page,
    but this is added protection against accidentally
    arriving at this page manually or by using "back.""

--- weeklydire_front_end/src/Components/PaypalBox.js
+++ weeklydire_front_end/src/Components/PaypalBox.js
@@ -1,6 +1,6 @@
 // Source: https://developer.paypal.com/studio/checkout/standard/integrate
 
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import { UserContext } from "../utils/UserContext";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { dbUrl } from "../utils/dbUrl";
@@ -11,7 +11,7 @@ function Message({ content }) {
 }
 
 
-const PaypalBox = () => {
+const PaypalBox = ({currentUser}) => {
     const initialOptions = {
         "client-id":
         "AbVAgM7FO8L2uvoXcTf8NrN_NUYlXo9qTQ_KDG_VFXazTJmpHgu7XzYHqA2QbghGrQvGHzMIZaz3qgqx",
@@ -23,116 +23,118 @@ const PaypalBox = () => {
         "data-sdk-integration-source": "developer-studio",
     };
 
-    const [currentUser] = useContext(UserContext);
     const [message, setMessage] = useState("");
 
+
   return (
-    <div className="App">
-      <PayPalScriptProvider options={initialOptions}>
-        <PayPalButtons
-          style={{
-            shape: "rect",
-            layout: "vertical",
-            color: "gold",
-            label: "paypal",
-          }}
-          createOrder={async () => {
-            try {
-              const response = await fetch(dbUrl + "/paypal/api/orders", {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                // use the "body" param to optionally pass additional order information
-                // like product ids and quantities
-                body: JSON.stringify({
-                  cart: [
-                    {
-                      id: "Permanent_Premium_Subscription",
-                      userInfo: {
-                        firstName: currentUser.firstName,
-                        lastName: currentUser.lastName,
-                        userId: currentUser._id
-                      }
-                    //   quantity: "YOUR_PRODUCT_QUANTITY"
-                    },
-                  ],
-                }),
-              });
-
-              const orderData = await response.json();
-
-              if (orderData.id) {
-                return orderData.id;
-              } else {
-                const errorDetail = orderData?.details?.[0];
-                const errorMessage = errorDetail
-                  ? `${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`
-                  : JSON.stringify(orderData);
-
-                throw new Error(errorMessage);
-              }
-            } catch (error) {
-              console.error(error);
-              setMessage(`Could not initiate PayPal Checkout... ${error}`);
-            }
-          }}
-          onApprove={async (
-            data,
-            actions
-          ) => {
-            try {
-              const response = await fetch(
-                `${dbUrl}/paypal/api/orders/${data.orderID}/capture/${currentUser._id}`,
-                {
-                  method: "POST",
-                  headers: {
+    <div className="paypal-box">
+        <PayPalScriptProvider options={initialOptions}>
+            <PayPalButtons
+            style={{
+                shape: "rect",
+                layout: "vertical",
+                color: "gold",
+                label: "paypal",
+            }}
+            createOrder={async () => {
+                try {
+                const response = await fetch(dbUrl + "/paypal/api/orders", {
+                    method: "POST",
+                    headers: {
                     "Content-Type": "application/json",
-                  },
+                    },
+                    // use the "body" param to optionally pass additional order information
+                    // like product ids and quantities
+                    body: JSON.stringify({
+                    cart: [
+                        {
+                        id: "Permanent_Premium_Subscription",
+                        userInfo: {
+                            firstName: currentUser.firstName,
+                            lastName: currentUser.lastName,
+                            userId: currentUser._id
+                        }
+                        //   quantity: "YOUR_PRODUCT_QUANTITY"
+                        },
+                    ],
+                    }),
+                });
+
+                const orderData = await response.json();
+
+                if (orderData.id) {
+                    return orderData.id;
+                } else {
+                    const errorDetail = orderData?.details?.[0];
+                    const errorMessage = errorDetail
+                    ? `${errorDetail.issue} ${errorDetail.description} (${orderData.debug_id})`
+                    : JSON.stringify(orderData);
+
+                    throw new Error(errorMessage);
                 }
-              );
-
-              const orderData = await response.json();
-              // Three cases to handle:
-              //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-              //   (2) Other non-recoverable errors -> Show a failure message
-              //   (3) Successful transaction -> Show confirmation or thank you message
-
-              const errorDetail = orderData?.details?.[0];
-
-              if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
-                // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-                // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
-                return actions.restart();
-              } else if (errorDetail) {
-                // (2) Other non-recoverable errors -> Show a failure message
-                throw new Error(
-                  `${errorDetail.description} (${orderData.debug_id})`
+                } catch (error) {
+                console.error(error);
+                setMessage(`Could not initiate PayPal Checkout... ${error}`);
+                }
+            }}
+            onApprove={async (
+                data,
+                actions
+            ) => {
+                try {
+                    console.log('current user id is: ');
+                    console.log(currentUser._ID)
+                const response = await fetch(
+                    `${dbUrl}/paypal/api/orders/${data.orderID}/capture/${currentUser._id}`,
+                    {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    }
                 );
-              } else {
-                // (3) Successful transaction -> Show confirmation or thank you message
-                // Or go to another URL:  actions.redirect('thank_you.html');
-                const transaction =
-                  orderData.purchase_units[0].payments.captures[0];
+
+                const orderData = await response.json();
+                // Three cases to handle:
+                //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+                //   (2) Other non-recoverable errors -> Show a failure message
+                //   (3) Successful transaction -> Show confirmation or thank you message
+
+                const errorDetail = orderData?.details?.[0];
+
+                if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
+                    // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+                    // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
+                    return actions.restart();
+                } else if (errorDetail) {
+                    // (2) Other non-recoverable errors -> Show a failure message
+                    throw new Error(
+                    `${errorDetail.description} (${orderData.debug_id})`
+                    );
+                } else {
+                    // (3) Successful transaction -> Show confirmation or thank you message
+                    // Or go to another URL:  actions.redirect('thank_you.html');
+                    const transaction =
+                    orderData.purchase_units[0].payments.captures[0];
+                    setMessage(
+                    `Transaction ${transaction.status}: ${transaction.id}. See console for all available details`
+                    );
+                    // console.log(
+                    //   "Capture result",
+                    //   orderData,
+                    //   JSON.stringify(orderData, null, 2)
+                    // );
+                }
+                } catch (error) {
+                console.error(error);
                 setMessage(
-                  `Transaction ${transaction.status}: ${transaction.id}. See console for all available details`
+                    `Sorry, your transaction could not be processed...${error}`
                 );
-                // console.log(
-                //   "Capture result",
-                //   orderData,
-                //   JSON.stringify(orderData, null, 2)
-                // );
-              }
-            } catch (error) {
-              console.error(error);
-              setMessage(
-                `Sorry, your transaction could not be processed...${error}`
-              );
-            }
-          }} 
-        />
-      </PayPalScriptProvider>
-      <Message content={message} />
+                }
+            }} 
+            />
+        </PayPalScriptProvider>
+        <Message content={message} />
     </div>
   );
 }
--- weeklydire_front_end/src/Pages/JoinUp.js
+++ weeklydire_front_end/src/Pages/JoinUp.js
@@ -1,28 +1,81 @@
 import {Container, Col, Row} from 'reactstrap';
 import PaypalBox from '../Components/PaypalBox';
+import { UserContext } from "../utils/UserContext";
+import { useContext } from 'react';
 
 const JoinUp = () => {
 
+        const [currentUser] = useContext(UserContext);
 
-  return (
-    <Container>
-        <Row>
-            <Col className='text-center'>
-                <h1>This content is for subscribers only</h1>
-                <h3>Enjoy premium content today by signing up for a subscription!</h3>
-                <p>This project uses the Paypal SDK to facilitate payments.</p>
-                <p>It's set up using Paypal's development sandbox, so no actual money is spent.</p>
-                <p>Go ahead and try making a payment!</p>
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-                <PaypalBox />
-            </Col>
-        </Row>
 
-    </Container>
-  )
+    // If the they're not logged in, have user sign in first
+    const NotSignedIn = () => {
+        return (
+            <>
+                <h2>We're excited to have you join our premium access team!</h2>
+                <p>Please sign in so we can double check if you already have premium access.</p>
+            </>
+        )
+    };
+
+    const AlreadyPremium = () => {
+        return (
+            <>
+                <h2>Oops! You're already a premium user!</h2>
+                <p>Thanks for supporing us. There's no need to sign up again</p>
+                <p>We're not sure how you got here, but there's no need to purchase your subscription again!</p>
+                <p>Try logging out then logging in again, and try accessing the premium content you were trying to view before.</p>
+            </>
+        )
+    }
+
+    const SignUpBox = () => {
+        return (
+            <>
+                <Row>
+                    <Col className='text-center'>
+                        <h1>This content is for subscribers only</h1>
+                        <h3>Enjoy premium content today by signing up for a subscription!</h3>
+                        <p>This project uses the Paypal SDK to facilitate payments.</p>
+                        <p>It's set up using Paypal's development sandbox, so no actual money is spent.</p>
+                        <p>Go ahead and try making a payment!</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <PaypalBox currentUser={currentUser}/>
+                    </Col>
+                </Row>
+            </>
+        )
+    }
+
+    let displayedBox;
+
+    switch (currentUser.premiumUser) {
+        case (undefined):
+            displayedBox = <NotSignedIn />
+            break;
+        case (false):
+            displayedBox = <SignUpBox />
+            break;
+        case (true):
+            displayedBox = <AlreadyPremium />;
+            break;
+        default:
+            console.log(`There was an error on "JoinUp". The currentUser.premiumUser value is ${currentUser.premiumUser}`)
+    }
+
+    console.log(currentUser);
+
+        return (
+            <Container>
+                {displayedBox}
+                {/* <AlreadyPremium /> */}
+                {/* <SignUpBox /> */}
+                {/* <NotSignedIn /> */}
+            </Container>
+        )
 }
 
 export default JoinUp
